04019fa6e88446f2ddc9087f60ffbcaaf5a9b3c8
vosuyak <vosyak@yahoo.com>
2017-10-27 21:32:28 -0500

    Value Calcultor with PHP validation

---
+++ 319L6Task_2/display_results.php
@@ -0,0 +1,76 @@
+<?php
+    // get the data from the form
+        $investment = filter_input(INPUT_POST, 'investment', FILTER_VALIDATE_FLOAT);
+        $interest_rate = filter_input(INPUT_POST, 'interest_rate', FILTER_VALIDATE_FLOAT);
+        $years = filter_input(INPUT_POST, 'years', FILTER_VALIDATE_INT);        
+    //date formating
+        $date = date('m/d/y');
+    // validate investment
+        if($investment === FALSE){
+            $error_message = 'Investment must be a valid number.';
+        } else if($investment <= 0){
+            $error_message = 'Investment must be greater than zero.';
+        }
+    // validate interest rate
+        else if($interest_rate === FALSE){
+            $error_message = 'Interest rate must be a valid number.';
+        } else if($interest_rate >= 15){
+            $error_message = 'Interest must be greater than less than or equal to 15.';
+        }
+    // validate years
+        else if($years === FALSE){
+            $error_message = 'Years must be a valid number.';
+        } else if($years <= 0){
+            $error_message = 'Years must be greater than zero.';
+        } else if($years > 30){
+            $error_message = 'Years must be less than 31.';
+        }
+    // set error message to empty string if no invalid entries
+        else{
+            $error_message = '';
+        }
+    // if an error message exists, go to the index page
+        if($error_message != ''){
+            include('index.php');
+            exit();
+        }
+    // calucate the future values
+        $future_value = $investment;
+        for($i  = 1; $i <= $years; $i++){
+            $future_value = $future_value + ($future_value * $interest_rate * .01);
+        }
+    // apply currency and percent formatting
+        $investment_f = '$'.number_format( $investment,2);
+        $yearly_rate_f = $interest_rate.'%';
+        $future_value_f = '$'.number_format($future_value,2);
+?>
+<!DOCTYPE html>
+<html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Future Value Calculator | Task 2</title>
+        <meta name="description" content="Calculate Future Value of an Item">
+        <meta name="keywords" content="calculate, value, item">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="./normalize.css">
+        <link rel="stylesheet" type="text/css" href="./main.css">
+    </head>
+    <body>
+    <!-- application out putting calculations -->
+        <main>
+            <h1>Future Value Calculator</h1>
+
+            <label>Investment Amount: </label>
+            <span><?php echo $investment_f?></span><br>
+            <label>Yearly Interest: </label>
+            <span><?php echo $yearly_rate_f?></span><br>
+            <label>Number of Years: </label>
+            <span><?php echo $years?></span><br>
+            <label>Future Value: </label>
+            <span><?php echo $future_value_f ?></span><br>
+        <!-- current date stamp -->
+            <?php echo 'This calculation was done on '.$date.'.' ?>
+        </main>
+    </body>
+</html>
---
+++ 319L6Task_2/index.php
@@ -0,0 +1,55 @@
+<?php
+    // set default value of variables for intial page load
+    if(!isset($investment)){
+        $investment = '';
+    }
+    if(!isset($interest_rate)){
+        $interest_rate = '';
+    }
+    if(!isset($years)){
+        $years = '';
+    }
+?>
+<!DOCTYPE html>
+<html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Future Value Calculator | Task 2</title>
+        <meta name="description" content="Calculate Future Value of an Item">
+        <meta name="keywords" content="calculate, value, item">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="./normalize.css">
+        <link rel="stylesheet" type="text/css" href="./main.css">
+    </head>
+    <body>
+        <!-- application out inputting calculations -->
+    <main>
+        <h1>Future Value Calculator</h1>
+        <?php if(!empty($error_message)) { ?>
+            <p class="error"><?php echo htmlspecialchars($error_message);?></p>
+        <?php } ?>
+        <form action="display_results.php" method="post">
+            <div id="data">
+                <label>Investment Amount: </label>
+                <input type="text" name="investment" value="<?php echo htmlspecialchars($investment);?>">
+                <br>
+
+                <label>Yearly Interest Rate: </label>
+                <input type="text" name="interest_rate" value="<?php echo htmlspecialchars($interest_rate);?>">
+                <br>
+
+                <label>Number of Years: </label>
+                <input type="text" name="years" value="<?php echo htmlspecialchars($years);?>">
+                <br>                
+            </div>
+        <!-- button submitting the application -->
+            <div id="buttons">
+                <label>&nbsp;</label>
+                <input type="submit" value="Calculate"><br>
+            </div>
+        </form>
+    </main>
+        
+    </body>
+</html>
